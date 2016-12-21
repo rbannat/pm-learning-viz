52e115f4d3954078c112bc35e1de17026cdacdff
René Bannat <rene.bannat@parlamind.com>
2016-12-21 11:47:36 +0100

    add first chart to dashboard

--- src/app/shared/customer-barchart/customer-barchart.component.ts
+++ src/app/shared/customer-barchart/customer-barchart.component.ts
@@ -1,12 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation, Input} from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
   selector: 'app-customer-barchart',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './customer-barchart.component.html',
   styleUrls: ['./customer-barchart.component.css']
 })
 export class CustomerBarchartComponent implements OnInit {
+
+  @Input() topX: number;
 
   loading = true;
 
@@ -14,29 +17,30 @@ export class CustomerBarchartComponent implements OnInit {
   }
 
   ngOnInit() {
-    var self = this;
+    let self = this;
 
-    d3.json("assets/customer-list.json", function (error, data) {
+    d3.json("assets/customer-list.json", function (error, data:any) {
       if (error) {
         self.loading = false;
         return console.warn(error);
       } else {
         self.loading = false;
-        console.log(data);
-        // data.sort(function (a, b) {
-        //   return b["updateCaseCount"] - a["updateCaseCount"];
-        // });
+        data.sort(function (a, b) {
+          return b["updateCaseCount"] - a["updateCaseCount"];
+        });
+        if(self.topX){data = data.slice(0,self.topX)}
         self.initChart(data);
       }
     });
   }
 
   initChart(data) {
+    console.log(data);
     let width = document.querySelectorAll('.d3-customer-barchart')[0].clientWidth,
       barHeight = 20,
       leftMargin = 100;
 
-    let chart = d3.select('.chart');
+    let chart = d3.select('.d3-customer-barchart');
     chart.html('<p class="lead">Number of Updatecases</p>');
 
     let svg = chart.append('svg')
