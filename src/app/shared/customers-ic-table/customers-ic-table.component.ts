f5cf30bc4cdb4d61886dafa212fba53a52cc63f2
René Bannat <rene.bannat@parlamind.com>
2017-01-19 16:24:37 +0100

    update matrix layout

--- src/app/shared/customers-ic-table/customers-ic-table.component.ts
+++ src/app/shared/customers-ic-table/customers-ic-table.component.ts
@@ -22,11 +22,11 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
   private indexCasesPromise: Promise<any>;
   private indexCases: IndexCase[];
   private loading: boolean = true;
-  private actionTypeColor:string = 'unknown';
+  private actionTypeColor: string = 'unknown';
 
   public orderBy: any = 'custcat';
-  private margin: any = {top: 50, bottom: 10, left: 100, right: 75};
-  private cellSize: number = 10;
+  private margin: any = {top: 100, bottom: 10, left: 150, right: 75};
+  private cellSize: number = 15;
   private legendElementWidth: number;
   private chart: any;
   private width: number;
@@ -34,10 +34,9 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
   private svg: any;
   private rowSortOrder: boolean;
   private colSortOrder: boolean;
-  private selectedLabel: any;
+  private selectedLabel: string;
 
   private updateCases: any;
-  private categories: any;
   private matrixData: any[];
   private colors: string[];
   private colorScale: any;
@@ -71,12 +70,22 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
         if (nameA > nameB) {
           return 1;
         }
-
         // names must be equal
         return 0;
       });
       this.updateCases = this.updateCaseService.getRealUpdateCases(customers);
-      this.categories = indexCases.sort((a, b) => a.id - b.id);
+      this.indexCases = indexCases.sort(function (a, b) {
+        let nameA = a.representative.toUpperCase(); // ignore upper and lowercase
+        let nameB = b.representative.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
 
       this.matrixData = this.getMatrixData();
 
@@ -101,8 +110,8 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
 
     let self = this;
     let element = this.chartContainer.nativeElement;
-    this.width = this.cellSize * this.categories.length;
-    this.height = this.cellSize * this.customers.length;
+    this.width = this.cellSize * this.customers.length;
+    this.height = this.cellSize * this.indexCases.length;
     this.legendElementWidth = this.cellSize * 2.5;
 
     this.colors = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c', '#08306b'];
@@ -112,7 +121,9 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
 
     this.svg = d3.select(element).append('svg')
       .attr("width", this.width + this.margin.left + this.margin.right)
-      .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .attr("height", this.height + this.margin.top + this.margin.bottom);
+
+    this.chart = this.svg
       .append("g")
       .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
   }
@@ -125,13 +136,16 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
 
     let self = this;
 
-    let rowLabels = this.svg.append("g")
+    let rowLabels = this.chart.append("g")
       .selectAll(".rowLabelg")
-      .data(this.customers)
+      .data(this.indexCases)
       .enter()
       .append("text")
       .text(function (d) {
-        return d.label;
+        if (d.representative.length > 25)
+          return d.representative.substring(0, 25) + '...';
+        else
+          return d.representative;
       })
       .attr("x", 0)
       .attr("y", (d, i) => {
@@ -149,20 +163,24 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
         d3.select(this).classed("text-hover", false);
       })
       .on("click", function (d, i) {
-        if (this.selectedLabel === 'r' + i) self.rowSortOrder = !self.rowSortOrder;
-        this.selectedLabel = 'r' + i;
+        if (self.selectedLabel === 'r' + i) self.rowSortOrder = !self.rowSortOrder;
+        self.selectedLabel = 'r' + i;
         self.sortbylabel("r", i, self.rowSortOrder);
-        this.orderBy = 'custom';
+        self.orderBy = 'custom';
         d3.select("#order").property('value', 'custom');
+      })
+      .append('title')
+      .text(function (d) {
+        return d.representative;
       });
 
-    let colLabels = this.svg.append("g")
+    let colLabels = this.chart.append("g")
         .selectAll(".colLabelg")
-        .data(this.categories)
+        .data(this.customers)
         .enter()
         .append("text")
         .text(function (d) {
-          return d.id;
+          return d.label;
         })
         .attr("x", 0)
         .attr("y", (d, i) => {
@@ -180,15 +198,15 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
           d3.select(this).classed("text-hover", false);
         })
         .on("click", function (d, i) {
-          if (this.selectedLabel === 'c' + i) self.colSortOrder = !self.colSortOrder;
-          this.selectedLabel = 'c' + i;
-          this.orderBy = 'custom';
+          if (self.selectedLabel === 'c' + i) self.colSortOrder = !self.colSortOrder;
+          self.selectedLabel = 'c' + i;
+          self.orderBy = 'custom';
           d3.select("#order").property('value', 'custom');
           self.sortbylabel("c", i, self.colSortOrder);
         })
       ;
 
-    let heatMap = this.svg.append("g")
+    let heatMap = this.chart.append("g")
         .attr("class", "g3")
         .selectAll(".cellg")
         .data(this.matrixData)
@@ -219,23 +237,23 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
           //Update the tooltip position and value
           let tooltip = d3.select("#tooltip")
             .style("left", (parseInt(d3.select(this).attr('x')) + self.margin.left + self.cellSize * 2) + "px")
-            .style("bottom", (self.height - (parseInt(d3.select(this).attr('y')))) + "px");
+            .style("top", (parseInt(d3.select(this).attr('y')) + self.margin.top - self.margin.top) + "px");
 
-          self.actionTypeColor = self.colorsService.getColor(self.categories[d.col].type);
+          self.actionTypeColor = self.colorsService.getColor(self.indexCases[d.row].type);
 
           tooltip.select("#customer")
-            .text("Customer: " + self.customers[d.row].label);
+            .text("Customer: " + self.customers[d.col].label);
 
           tooltip.select("#category")
-            .text("Index Case: " + self.categories[d.col].representative);
+            .text("Index Case: " + self.indexCases[d.row].representative);
 
           tooltip.select("#action-type")
-            .text(self.categories[d.col].type);
+            .text(self.indexCases[d.row].type);
 
           tooltip.select("#topic")
             .text(a => {
-              if (self.categories[d['col']].topic !== undefined) {
-                return "Topic: " + self.categories[d['col']].topic
+              if (self.indexCases[d['row']].topic !== undefined) {
+                return "Topic: " + self.indexCases[d['row']].topic
               } else {
                 return "Topic: " + 'No Topic';
               }
@@ -256,7 +274,7 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
       ;
 
     // Add a legend for the color values.
-    let legend = this.svg.selectAll(".legend")
+    let legend = this.chart.selectAll(".legend")
       .data(this.colorScale.ticks(6).slice(1).reverse())
       .enter().append("g")
       .attr("class", "legend")
@@ -275,7 +293,7 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
       .attr("dy", ".35em")
       .text(String);
 
-    this.svg.append("text")
+    this.chart.append("text")
       .attr("class", "label")
       .attr("x", this.width + 20)
       .attr("y", 10)
@@ -288,9 +306,8 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
   }
 
   // Change ordering of cells
-
   sortbylabel(rORc, i, sortOrder) {
-    let t = this.svg.transition().duration(2000);
+    let t = this.chart.transition().duration(1000);
     let log2r: any[] = [];
     let sorted; // sorted is zero-based index
 
@@ -298,8 +315,8 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
       return log2r.push(d['value'])
     });
 
-    if (rORc == "r") { // sort log2ratio of a customer
-      sorted = d3.range(this.categories.length).sort(function (a, b) {
+    if (rORc == "r") { // sort log2ratio of a indexCase
+      sorted = d3.range(this.customers.length).sort(function (a, b) {
         if (sortOrder) {
           return log2r[b] - log2r[a];
         } else {
@@ -316,8 +333,8 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
           return sorted.indexOf(i) * this.cellSize;
         })
       ;
-    } else { // sort log2ratio of a category
-      sorted = d3.range(this.customers.length).sort(function (a, b) {
+    } else { // sort log2ratio of a customer
+      sorted = d3.range(this.indexCases.length).sort(function (a, b) {
         if (sortOrder) {
           return log2r[b] - log2r[a];
         } else {
@@ -340,7 +357,7 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
   order(value) {
     this.orderBy = value;
     if (value == "custcat") {
-      let t = this.svg.transition().duration(3000);
+      let t = this.chart.transition().duration(1000);
       t.selectAll(".cell")
         .attr("x", (d) => {
           return (d.col) * this.cellSize;
@@ -360,18 +377,7 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
         });
 
     } else if (value == "cust") {
-      let t = this.svg.transition().duration(3000);
-      t.selectAll(".cell")
-        .attr("y", (d) => {
-          return (d.row) * this.cellSize;
-        });
-
-      t.selectAll(".rowLabel")
-        .attr("y", (d, i) => {
-          return i * this.cellSize;
-        });
-    } else if (value == "cat") {
-      var t = this.svg.transition().duration(3000);
+      let t = this.chart.transition().duration(1000);
       t.selectAll(".cell")
         .attr("x", (d) => {
           return (d.col) * this.cellSize;
@@ -382,6 +388,18 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
           return i * this.cellSize;
         })
       ;
+
+    } else if (value == "cat") {
+      let t = this.chart.transition().duration(1000);
+      t.selectAll(".cell")
+        .attr("y", (d) => {
+          return (d.row) * this.cellSize;
+        });
+
+      t.selectAll(".rowLabel")
+        .attr("y", (d, i) => {
+          return i * this.cellSize;
+        });
     }
   }
 
@@ -400,18 +418,18 @@ export class CustomersIcTableComponent implements OnInit, OnChanges {
     this.customers = _.filter(this.customers, customer => {
       return _.some(this.updateCases, uc => uc['customerId'] == customer['id']);
     });
-    this.categories = _.filter(this.categories, ic => {
+    this.indexCases = _.filter(this.indexCases, ic => {
       return _.some(this.updateCases, uc => uc['indexCaseId'] === ic['id']);
     });
 
+    let categoriesLength = this.indexCases.length;
     let customersLength = this.customers.length;
-    let categoriesLength = this.categories.length;
-    for (let i = 0; i < customersLength; i++) {
-      for (let j = 0; j < categoriesLength; j++) {
+    for (let i = 0; i < categoriesLength; i++) {
+      for (let j = 0; j < customersLength; j++) {
         matrixData.push({
           row: i,
           col: j,
-          value: _.filter(this.updateCases, updateCase => updateCase['customerId'] === this.customers[i].id && updateCase['indexCaseId'] === this.categories[j].id).length
+          value: _.filter(this.updateCases, updateCase => updateCase['indexCaseId'] === this.indexCases[i].id && updateCase['customerId'] === this.customers[j].id).length
         });
       }
     }
